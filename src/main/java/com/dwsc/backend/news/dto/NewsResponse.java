482b0a6a7cc21011155ded45cd5d0c46b467b765
ilias-chat <ilias.chat.7@gmail.com>
2026-05-28 06:09:16 +0200

    WIP: local changes before revert

--- src/main/java/com/dwsc/backend/news/dto/NewsResponse.java
+++
@@ -1,6 +0,0 @@
-package com.dwsc.backend.news.dto;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(name = "News")
-public record NewsResponse(String id, String title, String content, String date) {}
